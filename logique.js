951feb74c267e2fb1910bec0b4c548eefabe527e
Joman-12 <tchinguiloumanzama@gmail.com>
2025-01-27 15:54:03 +0000

    mvp done

--- logique.js
+++ logique.js
@@ -11,3 +11,32 @@ function display(id){
     console.log(id);
     
 }
+
+function openCommande(produit){
+    const modal = document.getElementById("modalProjet");
+    const modalText = document.getElementById("modalText");
+
+    // Afficher la modal
+    modal.style.display = "block";
+
+    // Réinitialiser le texte
+    modalText.textContent = "Qte de ";
+
+    
+    modalText.textContent += produit + ' :'
+          
+}
+
+// Fonction pour fermer la modal
+function closeModal() {
+    const modal = document.getElementById("modalProjet");
+    modal.style.display = "none";
+}
+
+// Fermer la modal si on clique en dehors de celle-ci
+window.onclick = function(event) {
+    const modal = document.getElementById("modalProjet");
+    if (event.target === modal) {
+        closeModal();
+    }
+}
